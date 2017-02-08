c03c3ba8410f8c2ed813265544cbf0b68e0e0462
Paige Busch <93paigedf@gmail.com>
2017-02-08 12:26:04 -0700

    prev next buttons

--- service.js
+++ service.js
@@ -1,10 +1,16 @@
 angular.module('userProfiles').service('mainService', function($http) {
 
-  this.getUsers = function() {
+  this.getUsers = function(pageNum) {
     return $http({
         method: 'GET',
-        url: 'https://reqres.in/api/users?page=1'
-    });
+        url: 'https://reqres.in/api/users?page=' + pageNum
+    }).then(function(response) {
+        if(response.status === 200) {
+            return response.data;
+        } else {
+            return "Error" + response.status;
+        }
+    })
   }
 
 });
